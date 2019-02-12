b86a0f45665eefb55b2c8164a7fc339dbbc8231b
gsandhu916 <gsandhu916@gmail.com>
2019-02-11 20:08:39 -0800

    player dealer and cards

--- app/app.js
+++ app/app.js
@@ -2,12 +2,76 @@
 Init app
 interact with DOM
 interact with localstorage
-
+1. player has money and can bet it
+2. player can hold cards
+3. dealer
  */
+let Player = function(name, money) {
+  this.name = name;
+  this.money = money;
+  this.hand = [];
+}
+Player.prototype = {
+  shuffle: function(cards) {
+    let usedCards = new Set();
+    let newCards = [];
+    while (cards.length > 0) {
+      let shuffleCards = Math.floor(Math.random() * cards.length);
+      let choosenCard = cards[shuffleCards];
+      if (!usedCards.has(shuffleCards)) {
+          newCards.push(choosenCard);
+          cards.splice(shuffleCards, 1);
+      } else {
+        continue;
+      }
+    }
+    return newCards;
+  }
+}
+
+let Card = function(suit, value) {
+  //0 = hearts, 1 = spades, 2 = dimonds, 3 = clubs
+  this.suit = suit; 
+  this.value = value;
+}
+Card.prototype = {
+  calculateWorth: function() {
+    if (this.value >= 10 && this.value <= 13) {
+      return 10;
+    }
+    else if (this.value === 14) {
+      return 1;
+    } else {
+      return this.value;
+    }
+  }
+}
+let cards = [];
+
+for (let x = 0; x <= 3; x++) {
+  for (let i = 2; i <= 14; i++) {
+    const newCard = new Card(x, i);
+    cards.push(newCard)
+  }
+}
 
 $(document).ready(function(){
   // this is where we jquery
   //var keyData = 'ourKey'; // going to need to make this dynamic?
+  const player = new Player('Sonu', 100);
+  const dealer = new Player('Dealer', 1000);
+  const deck = dealer.shuffle(cards);
+  console.log(deck);
+  player.hand.push(deck.pop());
+  player.hand.push(deck.pop());
+  dealer.hand.push(deck.pop());
+  dealer.hand.push(deck.pop());
+  console.log('player place bet')
+  const bet = 50;
+  if (player.money > bet) {
+    console.log(dealer.hand[1])
+    console.log(player.hand, 'players hand')
+  }
 
 
   $('.btn-add').on('click', function(e){
